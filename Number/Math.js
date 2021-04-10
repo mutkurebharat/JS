4ebb165f968987773e7f1ccb1ede66f527d650f5
BharatM <mutkurebharat@gmail.com>
2021-04-10 20:30:39 +0530

    Number Methods and Math Object

---
+++ Number/Math.js
@@ -0,0 +1,33 @@
+// var pi = Math.PI;
+
+// console.log(pi);
+
+// var num = 1.54;
+
+// console.log(Math.round(num)); // To Round up the value in a single digit;
+
+var num = -45.9;
+
+// console.log(Math.ceil(num)); // Returns nearest up intiger
+
+console.log(Math.floor(-45.9)); // Return nearest down intiger(or removes decimal value)
+
+// console.log(Math.trunc(num)) // Returns only intiger part of your number
+
+// console.log(Math.sign(num)) // Returns either your number positve, negative or null
+
+
+// console.log(Math.pow(3, 4)) // To cal culate Power of a number
+// console.log(Math.sqrt(4));
+
+// console.log(Math.abs(num))
+
+// console.log(Math.sin(Math.PI))
+
+// console.log(Math.min(10, 20, 30 , 5, 80))
+
+// console.log(Math.max(10, 20, 30, 5, 80));
+
+// console.log(Math.floor(Math.random() * 10));
+
+
---
+++ Number/numMethods.js
@@ -0,0 +1,34 @@
+var num = 1234;
+
+// var result = num.toString(); // To convert number to string 
+
+// console.log(typeof result)
+
+// var result = num.toExponential();
+
+// console.log(result)
+
+// var num1 = 12345.578;
+
+// var result = num1.toFixed(5) // To adjust the digits after decimal point
+
+// console.log(result);
+
+// var result = num1.toPrecision(6); // 
+
+// console.log(result);
+
+var a = "10.11";
+var b = "20.11";
+
+var c = Number(a);
+
+// console.log(typeof c)
+
+// console.log(parseInt(a) + parseInt(b));
+
+// console.log(Number(a) + Number(b));
+
+// console.log(Number.MIN_VALUE)
+
+console.log(-1/0);
